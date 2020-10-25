b0e7d5e31e8a02918ef2a4cc060642ee936b9dee
SEAN LEE <seanlee@MacBook-Pro.local>
2020-10-24 19:55:06 -0500

    setting meta data - front page

---
+++ content/blog/TIL/test1.md
@@ -0,0 +1,8 @@
+---
+title: 'Test blogging'
+date: 2020-10-24 16:21:13
+category: 'TIL'
+draft: false
+---
+
+test blogging
--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  title: `Bee starter`,
+  title: `Sean's Blog`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  author: `SEAN SANGHYUN LEE`,
+  introduction: `TIL, TWIL, Information I picked, My Portfolio, My Interest..etc`,
+  siteUrl: `https://shyunlee.netlify.app`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
-    medium: ``, // Your Medium account
+    github: `https://github.com/shyunlee`, // Your GitHub account
+    medium: `https://medium.com/@shyunlee0909`, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
   },
-  icon: `content/assets/felog.png`, // Add your favicon
+  icon: `content/assets/halfmoon.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'shyunlee/sean-gatsby-blog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
